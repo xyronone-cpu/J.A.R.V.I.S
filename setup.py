734fa4488cb9637ee4dc5293528006564257b103
xyronone-cpu <xyronone@gmail.com>
2026-09-12 09:03:43 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ if platform.system() == "Windows":
             f'    "{sys.executable}" "{postinstall}" -install\n'
         )
 
-print("\n✅ Setup complete! Run 'python main.py' to start MARK LII.")
+print("\n✅ Setup complete! Run 'python main.py' to start J.A.R.V.I.S.")
 
